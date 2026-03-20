9c83ce1c3937f759158f10f60cfd82395a5410c7
ThanusanS <thanusan0411@gmail.com>
2026-03-20 17:20:25 +0530

    Added File

---
+++ jsassync.js
@@ -0,0 +1,32 @@
+       
+       
+       const myPromise = new Promise((resolve, reject) => {
+        let success = true;
+
+        if (success) {
+            resolve("Operation successful");
+        } else {
+            reject("Operation failed");
+        }
+        });
+
+        myPromise.then((message) => {
+        console.log(message);
+        })
+
+        .catch((error) => {
+        console.error(error);
+        }
+        );
+
+
+        
+
+
+
+
+
+        
+
+
+
